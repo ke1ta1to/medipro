c867ecebfaf0c79aa76884b9fdb235f116099502
nukayorokobinoniwa <yuumukuda@gmail.com>
2024-11-13 17:09:54 +0900

    背景色を青に変更

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -1,12 +1,14 @@
 package view;
 
 import javax.swing.JFrame;
+import java.awt.Color;
 
 public class MainFrame extends JFrame {
 
   public MainFrame() {
     setTitle("Test Game");
     setSize(800, 600);
+    getContentPane().setBackground(Color.BLUE);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setVisible(true);
   }
