633499262129531647e404585d674c20d6c9f3fc
nukayorokobinoniwa <yuumukuda@gmail.com>
2024-11-13 16:47:22 +0900

    Test Gameにgame titleを変更

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 public class MainFrame extends JFrame {
 
   public MainFrame() {
-    setTitle("Game Title");
+    setTitle("Test Game");
     setSize(800, 600);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setVisible(true);
