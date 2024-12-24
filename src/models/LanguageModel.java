5972c3e6860948abee0f9bdcf1b3a3d2f36bc50a
nukayorokobinoniwa <152946230+nukayorokobinoniwa@users.noreply.github.com>
2024-12-24 13:06:03 +0900

    feat: waitとmove系,jumpのコンパイル処理の追加 (#13)

--- src/models/LanguageModel.java
+++ src/models/LanguageModel.java
@@ -1,20 +1,50 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LanguageModel {
+    private Pattern waitPattern = Pattern.compile("^wait\\s+(\\d+)$");
+
+    /**
+     * waitが来るまでif文で処理を取り、waitが来たらEntityStatusModelをAddする。
+     * splitする必要のないものを先に処理し、splitする必要がある場合はsplitして処理する。
+     */
 
     public void load(List<EntityStatusModel> stateLayers, String command) {
         // TODO: 言語の解析（メジェドさん）
+        boolean isMoveLeft = false;
+        boolean isMoveRight = false;
 
         stateLayers.clear();
-        String[] commands = command.split("\n");
+        List<String> commands = new ArrayList<>(Arrays.asList(command.split("\n")));
+        commands.add("wait 0");
         for (String c : commands) {
-            String[] params = c.split(" ");
-            int waiting = Integer.parseInt(params[0]);
-            boolean moveRight = Boolean.parseBoolean(params[1]);
-            boolean moveLeft = Boolean.parseBoolean(params[2]);
-            stateLayers.add(new EntityStatusModel(waiting, moveRight, moveLeft));
+            c = c.trim();
+            Matcher waitMatcher = waitPattern.matcher(c);
+            if (c.equals("move left")) {
+                isMoveLeft = true;
+            } else if (c.equals("move right")) {
+                isMoveRight = true;
+            } else if (c.equals("jump")) {
+                // TODO: jump処理
+            } else if (c.equals("stop")) {
+                isMoveLeft = false;
+                isMoveRight = false;
+            } else if (c.equals("hook")) {
+                // TODO: hook処理
+            } else if (waitMatcher.matches()) {
+                int waiting = Integer.parseInt(waitMatcher.group(1));
+                stateLayers.add(new EntityStatusModel(waiting, isMoveRight, isMoveLeft));
+                isMoveLeft = false;
+                isMoveRight = false;
+            } else {
+                System.out.println("error");
+                // TODO: 言語のコンパイルに失敗したときの処理
+            }
         }
     }
 
