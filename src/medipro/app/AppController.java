2a44baa1e63f4216f85c50778ca2d3c02c20e61f
Keita Ito <eguchi@eguchi.cc>
2025-01-21 00:29:30 +0900

    refactor: ゲームコントロール部分のリファクタ

--- src/medipro/app/AppController.java
+++ src/medipro/app/AppController.java
@@ -1,21 +1,11 @@
 package medipro.app;
 
-import java.awt.CardLayout;
-
-import javax.swing.JPanel;
-
-import medipro.cardobserver.CardSubject;
-
 public class AppController {
 
     private final AppModel model;
-    private final CardLayout cardLayout;
-    private final JPanel mainPanel;
 
-    public AppController(AppModel model, CardLayout cardLayout, JPanel mainPanel) {
+    public AppController(AppModel model) {
         this.model = model;
-        this.cardLayout = cardLayout;
-        this.mainPanel = mainPanel;
     }
 
     public AppModel getModel() {
---
+++ src/medipro/stage_menu_bar/StageMenuBarController.java
@@ -0,0 +1,41 @@
+package medipro.stage_menu_bar;
+
+import java.awt.event.ActionListener;
+
+import medipro.App;
+import medipro.subjects.CardSubject;
+import medipro.subjects.WorldSubject;
+
+public class StageMenuBarController {
+
+    private final StageMenuBarModel model;
+
+    public StageMenuBarController(StageMenuBarModel model) {
+        this.model = model;
+    }
+
+    public StageMenuBarModel getModel() {
+        return model;
+    }
+
+    public ActionListener handleGoToTop() {
+        return e -> {
+            CardSubject.setCardNumber(App.TOP_VIEW);
+        };
+    }
+
+    public ActionListener handleGoToStageLevel1() {
+        return e -> {
+            WorldSubject.setWorld(App.worldLevel1);
+            CardSubject.setCardNumber(App.GAME_VIEW);
+        };
+    }
+
+    public ActionListener handleGoToStageLevel2() {
+        return e -> {
+            WorldSubject.setWorld(App.worldLevel2);
+            CardSubject.setCardNumber(App.GAME_VIEW);
+        };
+    }
+
+}
