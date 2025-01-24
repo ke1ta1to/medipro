0b223a2920f7759a1578ada9a2cbd1389da885e1
Keita Ito <eguchi@eguchi.cc>
2025-01-24 16:01:34 +0900

    feat: デバッグメニューの作成

--- src/medipro/menu_bar/MenuBarView.java
+++ src/medipro/menu_bar/MenuBarView.java
@@ -14,20 +14,30 @@ public class MenuBarView extends JMenuBar {
         this.controller = controller;
 
         JMenu gameMenu = new JMenu("りさプロ");
-
-        add(gameMenu);
-
         JMenuItem goTitleMenu = new JMenuItem("タイトルへ戻る");
         goTitleMenu.addActionListener((e) -> {
             controller.handleGoTitle();
         });
         gameMenu.add(goTitleMenu);
-
         JMenuItem exitMenuItem = new JMenuItem("終了");
         exitMenuItem.addActionListener((e) -> {
             controller.handleExit();
         });
         gameMenu.add(exitMenuItem);
+        add(gameMenu);
+
+        JMenu debugMenu = new JMenu("デバッグ");
+        JMenu changeStageMenu = new JMenu("ステージ変更");
+        for (int i = 1; i <= 8; i++) {
+            JMenuItem changeStageMenuItem = new JMenuItem("ステージ" + i);
+            int stageNumber = i;
+            changeStageMenuItem.addActionListener((e) -> {
+                controller.handleChangeStage(stageNumber);
+            });
+            changeStageMenu.add(changeStageMenuItem);
+        }
+        debugMenu.add(changeStageMenu);
+        add(debugMenu);
     }
 
     public MenuBarModel getModel() {
