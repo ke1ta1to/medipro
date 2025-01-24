0bb9dda18071af953c4bb8c2c17ecb1c617a6172
Makura on CED <src1258372452486242304@gmail.com>
2025-01-24 14:13:44 +0900

    Add:testImage

--- src/medipro/tiles/WallTile.java
+++ src/medipro/tiles/WallTile.java
@@ -1,9 +1,9 @@
 package medipro.tiles;
 
-import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
 
-import medipro.World;
+import javax.swing.ImageIcon;
 
 public class WallTile extends Tile {
 
@@ -11,13 +11,15 @@ public class WallTile extends Tile {
         super(x, y);
     }
 
+    private Image loadImage(String name) {
+        return new ImageIcon(getClass().getResource(
+                "/medipro/images/" + name)).getImage();
+    }
+
     @Override
     public void draw(Graphics g) {
-        g.setColor(Color.GRAY);
-        g.fillRect(getX(), getY(), World.TILE_SIZE, World.TILE_SIZE);
-
-        g.setColor(Color.BLACK);
-        g.drawRect(getX(), getY(), World.TILE_SIZE, World.TILE_SIZE);
+        private final Image wallImage = loadImage("WallTest.png");
+        g.drawImage(wallImage, getX(), getY(), null);
     }
 
     @Override
