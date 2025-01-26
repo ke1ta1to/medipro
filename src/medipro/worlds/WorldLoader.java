dfe3ec5c25f58766a4582ece9c7d181a50129ab3
Keita Ito <eguchi@eguchi.cc>
2025-01-26 20:49:42 +0900

    refactor: ワールドのロード処理を整理し、パスを更新

--- src/medipro/worlds/WorldLoader.java
+++ src/medipro/worlds/WorldLoader.java
@@ -5,11 +5,12 @@ import medipro.stage.StageModel;
 
 public class WorldLoader {
 
-    public static World loadWorld(StageModel model, String basePath) {
+    public static World loadWorld(StageModel model, String baseDir) {
         World world = model.loadWorld(
-                App.class.getClassLoader().getResourceAsStream("medipro/world_templates/" + basePath + ".txt"),
+                App.class.getClassLoader().getResourceAsStream("medipro/world_templates/" + baseDir + "/world.txt"),
                 App.class.getClassLoader()
-                        .getResourceAsStream("medipro/world_templates/" + basePath + "_example_command.txt"));
+                        .getResourceAsStream("medipro/world_templates/" + baseDir + "/example_input.txt"));
         return world;
     }
+
 }
