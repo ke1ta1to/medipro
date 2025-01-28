5d8d3dfc85405eb0e42146d535171d967dfae252
Keita Ito <eguchi@eguchi.cc>
2025-01-28 17:25:07 +0900

    fix: ドメインを設定

--- infra/bin/medipro-server.ts
+++ infra/bin/medipro-server.ts
@@ -1,8 +1,19 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
 import { MediproServerStack } from "../lib/medipro-server-stack";
+import { MediproServerAppStack } from "../lib/medipro-server-app-stack";
 
 const app = new cdk.App();
 new MediproServerStack(app, "MediproServerStack", {
   repo: "eguchi1611/medipro",
+});
+
+new MediproServerAppStack(app, "MediproServerAppStack", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+  certificateArn:
+    "arn:aws:acm:us-east-1:183295441800:certificate/8236b35e-4de2-463d-a9e9-be3539deb9b7",
+  domainName: "api.medipro.keitaito.net",
 });
--- infra/lib/medipro-server-stack.ts
+++ infra/lib/medipro-server-stack.ts
@@ -2,7 +2,6 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as ecr from "aws-cdk-lib/aws-ecr";
 import * as iam from "aws-cdk-lib/aws-iam";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 interface MediproServerStackProps extends cdk.StackProps {
   repo: string;
