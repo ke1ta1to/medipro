326d1797182f16499eff8170ce5b8b60811929c6
Keita Ito <eguchi@eguchi.cc>
2025-01-28 11:15:54 +0900

    feat: ECRリポジトリとIAMロールの設定を追加

--- infra/lib/medipro-server-stack.ts
+++ infra/lib/medipro-server-stack.ts
@@ -1,16 +1,49 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ecr from "aws-cdk-lib/aws-ecr";
+import * as iam from "aws-cdk-lib/aws-iam";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
+interface MediproServerStackProps extends cdk.StackProps {
+  repo: string;
+}
+
 export class MediproServerStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: MediproServerStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const repository = new ecr.Repository(this, "MediproServerRepository", {
+      repositoryName: "medipro-server",
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      emptyOnDelete: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MediproServerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const role = new iam.Role(this, "MediproServerEcrRole", {
+      assumedBy: new iam.FederatedPrincipal(
+        `arn:aws:iam::${this.account}:oidc-provider/token.actions.githubusercontent.com`,
+        {
+          StringLike: {
+            "token.actions.githubusercontent.com:sub": `repo:${props.repo}:*`,
+          },
+          StringEquals: {
+            "token.actions.githubusercontent.com:aud": "sts.amazonaws.com",
+          },
+        },
+        "sts:AssumeRoleWithWebIdentity",
+      ),
+      roleName: "medipro-server-ecr-role",
+    });
+    role.addToPolicy(
+      new iam.PolicyStatement({
+        actions: ["ecr:GetAuthorizationToken"],
+        resources: ["*"],
+      }),
+    );
+    role.addToPolicy(
+      new iam.PolicyStatement({
+        actions: ["ecr:*"],
+        resources: [repository.repositoryArn],
+      }),
+    );
   }
 }
