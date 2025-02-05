6ef0d09e232e6c43e6e52199040476dd046555ce
Keita Ito <eguchi@eguchi.cc>
2025-02-05 17:59:28 +0900

    fix: ACL設定を削除

--- infra/lib/medipro-server-app-stack.ts
+++ infra/lib/medipro-server-app-stack.ts
@@ -50,11 +50,6 @@ export class MediproServerAppStack extends cdk.Stack {
         uid: "0",
         gid: "0",
       },
-      createAcl: {
-        ownerGid: "0",
-        ownerUid: "0",
-        permissions: "755",
-      },
     });
 
     const repository = ecr.Repository.fromRepositoryName(
