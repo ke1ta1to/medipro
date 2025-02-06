efdced84c66cf98b83fcf9b7532d8efa21f5afdb
nukayorokobinoniwa <yuumukuda@gmail.com>
2025-02-07 01:10:55 +0900

    feat:遊び方画面追加

--- src/net/keitaito/medipro/howtoplay/HowToPlayPage2View.java
+++ src/net/keitaito/medipro/howtoplay/HowToPlayPage2View.java
@@ -1,8 +1,9 @@
 package net.keitaito.medipro.howtoplay;
 
 import java.awt.BorderLayout;
-import java.awt.Font;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -11,12 +12,17 @@ public class HowToPlayPage2View extends JPanel {
     private final HowToPlayPage2Model model;
     private final HowToPlayPage2Controller controller;
 
+    private final Image image;
+
     public HowToPlayPage2View(HowToPlayPage2Model model, HowToPlayPage2Controller controller) {
         this.model = model;
         this.controller = controller;
 
-        JLabel titleLabel = new JLabel("testPage2", JLabel.CENTER);
-        titleLabel.setFont(new Font("Arial", Font.BOLD, 36));
+        ImageIcon icon = new ImageIcon(getClass().getClassLoader().getResource(
+                "net/keitaito/medipro/howtoplay/howtoplayimages/HowToPlay2.PNG"));
+        image = icon.getImage().getScaledInstance(700, 400, Image.SCALE_SMOOTH);
+
+        JLabel titleLabel = new JLabel(new ImageIcon(image), JLabel.CENTER);
         add(titleLabel, BorderLayout.NORTH);
     }
 
